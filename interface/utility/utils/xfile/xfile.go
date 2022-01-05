ef51043cc4b9ad216dd1948d04d6228dd7c1c7b4
1211sciel <1211sciel@gmail.com>
2022-01-05 21:26:33 +0800

    添加字自动生成代码说明不

--- interface/utility/utils/xfile/xfile.go
+++ interface/utility/utils/xfile/xfile.go
@@ -1,22 +1,22 @@
 package xfile
 
 type TemplateConfig struct {
-	RootPath      string
-	FileName      string // u_login_log
-	EntityName    string // LoginLog
-	PathService   string // /home/howl/learn/ciel-admin/interface/internal/service
-	PathApi       string // /home/howl/learn/ciel-admin/interface/apiv1
-	PathCmd       string // /home/howl/learn/ciel-admin/interface/internal/cmd/admin.go
-	PathDataFetch string // /home/howl/learn/ciel-admin/admin/data
-	PathIndex     string // /home/howl/learn/ciel-admin/admin/pages
-	group         string //  user
-	P1            string
-	P1Name        string
-	P2            string
-	P2Name        string
-	P3            string
-	P3Name        string
-	NameZh        string
+	RootPath      string // 项目绝对路径地址 eg :/home/howl/learn/ciel-admin
+	FileName      string // 文件名称 u_login_log
+	EntityName    string // 实体名称 LoginLog
+	PathService   string // 服务层文件夹 eg: /interface/apiv1/user_api.go
+	PathApi       string // 请求实体文件夹  eg:/home/howl/learn/ciel-admin/interface/apiv1
+	PathCmd       string // Cmd文件夹 /interface/internal/cmd/admin.go
+	PathDataFetch string // js axios 请求文件(没有请先创建) /admin/data/user.js
+	PathIndex     string // js 页面 /admin/pages/user
+	group         string // axios 分组名称 user
+	P1            string // 主要字段1 field
+	P1Name        string // 主要字段1 名称
+	P2            string // 主要字段2 field
+	P2Name        string // 主要字段2 名称
+	P3            string // 主要字段3 field
+	P3Name        string // 主要字段3 名称
+	NameZh        string // 页面标题
 }
 
 func Gen(c TemplateConfig) {
